e8012aa915cf40f06f00d63aa7b3e0d66d887aa4
Usama Elsayed <usamaelsayed101@gmail.com>
2021-11-28 21:25:37 +0200

    modified

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ const err = "برجاء ادخال تاريخ صحيح";
 
 formEdd.addEventListener("submit", (e) => {
   let d = new Date(collectedDate.value);
-  let delivery = new Date();
   let gestYear = d.getFullYear();
   let gestMonth = d.getMonth() + 1;
   let gestDay = d.getDate();
-  delivery.setDate(gestDay + 280);
+  d.setDate(gestDay + 280);
+  let now = new Date().getMonth() + 1;
+  let delivery = new Date(d);
   let resultYear = delivery.getFullYear();
   let resultMonth = delivery.getMonth() + 1;
   let resultDay = delivery.getDate();
-  if (collectedDate.value.length === 0) {
+
+  if (collectedDate.value.length === 0 || Math.floor(now - gestMonth) > 9) {
     document.querySelector(".results-field").value = err;
   } else {
     let results = ` يوم ${resultDay} شهر ${resultMonth} سنة ${resultYear}`;
